79cbba117035e4bc334be8a2c5730bc12fb03955
Sigrun Olafsdottir <solafsdottir@gmail.com>
2021-03-30 10:05:24 +0200

    Adding Dealership and CarSalesMan

---
+++ src/Models/Dealership.java
@@ -0,0 +1,14 @@
+package Models;
+
+public class Dealership {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

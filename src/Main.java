438d4f6e87b5dd635d2f9540611c442dfbcc0eff
Sigrun Olafsdottir <solafsdottir@gmail.com>
2021-03-30 09:44:57 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import Models.Bil;
+
+public class Main {
+
+
+    public Main() {
+        Bil b = new Bil();
+        Bil b2 = new Bil();
+        System.out.println("vi skapar 2 bilar");
+    }
+
+
+
+    public static void main(String[] args){
+       Main m = new Main();
+    }
+}
---
+++ src/Models/Bil.java
@@ -0,0 +1,23 @@
+package Models;
+
+public class Bil {
+
+    private String regnum;
+    private String color;
+
+    public String getRegnum() {
+        return regnum;
+    }
+
+    public void setRegnum(String regnum) {
+        this.regnum = regnum;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
